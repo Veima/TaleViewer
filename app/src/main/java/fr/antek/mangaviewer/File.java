a237f03b60a9a3c5e88d8ead1e0357750720598b
antoi <antoine.tekieli@gmail.com>
2023-09-23 14:01:05 +0200

    rework file sort

--- app/src/main/java/fr/antek/mangaviewer/File.java
+++ app/src/main/java/fr/antek/mangaviewer/File.java
@@ -3,6 +3,8 @@ package fr.antek.mangaviewer;
 
 import androidx.documentfile.provider.DocumentFile;
 
+import java.util.ArrayList;
+
 public class File implements Comparable<File>{
     private final String name;
     private final String path;
@@ -70,7 +72,74 @@ public class File implements Comparable<File>{
         }
     }
     public int compareTo(File file){
-        return this.name.compareTo(file.getName());
+        ArrayList<Object> splitTitleThis = this.splitIntAndString();
+        ArrayList<Object> splitTitleOther = file.splitIntAndString();
+        for (int i =0; i<splitTitleThis.size() ;i++) {
+            if (splitTitleOther.size()<=i) {
+                return 1;
+            }
+            if (!(splitTitleThis.get(i).equals(splitTitleOther.get(i)))) {
+                if (i%2 == 0) {
+                    return ((String)splitTitleThis.get(i)).compareTo((String)splitTitleOther.get(i));
+                }else {
+                    return (int) (((Double)splitTitleThis.get(i))-((Double)splitTitleOther.get(i)));
+                }
+            }
+        }
+        return -1;
+    }
+
+
+    public ArrayList<Object> splitIntAndString() {
+        String nameWithoutExp = this.getNameWithoutExp();
+        ArrayList<Object> outputList = new ArrayList<>();
+        int i=0;
+        char c=nameWithoutExp.charAt(i);
+        while (i<nameWithoutExp.length()) {
+
+            StringBuilder currentString = new StringBuilder();
+            while (!Character.isDigit(c)) {
+                currentString.append(c);
+                i+=1;
+                if (i>=nameWithoutExp.length()) {
+                    outputList.add(currentString.toString());
+                    return outputList;
+                }else {
+                    c=nameWithoutExp.charAt(i);
+                }
+
+            }
+            outputList.add(currentString.toString());
+
+            StringBuilder currentInt = new StringBuilder();
+            while (Character.isDigit(c)) {
+                currentInt.append(c);
+                i+=1;
+                if (i>=nameWithoutExp.length()) {
+                    outputList.add(Double.parseDouble(currentInt.toString()));
+                    return outputList;
+                }else {
+                    c=nameWithoutExp.charAt(i);
+                }
+            }
+            outputList.add(Double.parseDouble(currentInt.toString()));
+
+        }
+        return outputList;
+    }
+
+    public String getNameWithoutExp() {
+        if (this instanceof Directory){
+            return name;
+        }else{
+            int lastPointIndex = name.lastIndexOf(".");
+
+            if (lastPointIndex >= 0) {
+                return name.substring(0, lastPointIndex);
+            } else {
+                return name;
+            }
+        }
     }
     public boolean equals(File file) {
         return (this.path.equals(file.getPath()) && this.name.equals(file.getName()));
