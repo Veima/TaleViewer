5b2939d1162aa284556a215e4edb06a42e2ae7e8
antoine <antoine.tekieli@gmail.com>
2023-09-25 16:15:33 +0200

    parameter avtivity creation

---
+++ app/src/main/java/fr/antek/mangaviewer/ParameterActivity.java
@@ -0,0 +1,14 @@
+package fr.antek.mangaviewer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ParameterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parameter);
+    }
+}
