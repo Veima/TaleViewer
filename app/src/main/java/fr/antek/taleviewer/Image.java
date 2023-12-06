f158c0d9525aa4b4572cbf8012dfea0f2d7fcbf3
antoine <antoine.tekieli@gmail.com>
2023-12-06 12:03:06 +0100

    handle corrupted image

--- app/src/main/java/fr/antek/taleviewer/Image.java
+++ app/src/main/java/fr/antek/taleviewer/Image.java
@@ -1,13 +1,19 @@
 package fr.antek.taleviewer;
 
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.net.Uri;
 import android.provider.MediaStore;
+import android.text.Layout;
+import android.text.StaticLayout;
+import android.text.TextPaint;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.documentfile.provider.DocumentFile;
 
 import java.io.IOException;
+
 
 /**
  * Represents an image file in the application's file system, extending the File class.
@@ -37,8 +43,13 @@ public class Image extends File{
     public void open() {
         try {
             bitmapRaw = MediaStore.Images.Media.getBitmap(super.getActivity().getContentResolver(), super.getDoc().getUri());
+            if (bitmapRaw == null){
+                bitmapRaw = generateTextBitmap(getDoc().getName() + " " + getActivity().getString(R.string.ErrorOpen), 720, 1280);
+            }
             isWide = bitmapRaw.getWidth()>bitmapRaw.getHeight();
             isOpen = true;
+
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -62,6 +73,37 @@ public class Image extends File{
             open();
         }
         return bitmapRaw;
+    }
+
+    /**
+     * Generates a bitmap with specified width and height, containing centered text.
+     * This function creates a bitmap with a black background and renders the given text in white color
+     * at the center of the bitmap. The text will be automatically wrapped onto multiple lines
+     * if it exceeds the specified width.
+     *
+     * @param text   The text to be rendered on the bitmap.
+     * @param width  The width of the generated bitmap.
+     * @param height The height of the generated bitmap.
+     * @return A Bitmap object containing the rendered text.
+     */
+    public static Bitmap generateTextBitmap(String text, int width, int height) {
+        Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+
+        canvas.drawColor(Color.BLACK);
+
+        TextPaint textPaint = new TextPaint();
+        textPaint.setColor(Color.WHITE);
+        textPaint.setTextSize(30);
+
+        StaticLayout staticLayout = new StaticLayout(text, textPaint, width, Layout.Alignment.ALIGN_CENTER, 1.0f, 0.0f, false);
+
+        canvas.save();
+        canvas.translate(0, (float) ((height - staticLayout.getHeight()) / 2.0));
+        staticLayout.draw(canvas);
+        canvas.restore();
+
+        return bitmap;
     }
 
     public Uri getUri(){
