be6a2176e3dd4ce05cd9e8dddbccf765bbfa1420
antoi <antoine.tekieli@gmail.com>
2024-02-18 19:44:38 +0100

    1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "fr.antek.taleviewer"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 4
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
